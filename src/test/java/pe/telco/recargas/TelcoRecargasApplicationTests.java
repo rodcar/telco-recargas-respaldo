a9ccbfb756372935aa492cb79296974095eff70e
rodcar <ivanrod12@hotmail.com>
2023-01-04 20:52:35 -0500

    commit inicial

---
+++ src/test/java/pe/telco/recargas/TelcoRecargasApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.telco.recargas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelcoRecargasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
