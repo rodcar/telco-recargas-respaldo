a7265c30a52a81cdabf61f17892f9bba7d1bd940
rodcar <ivanrod12@hotmail.com>
2023-01-07 23:50:12 -0500

    configura como cliente de Eureka

--- src/main/java/pe/telco/recargas/TelcoRecargasApplication.java
+++ src/main/java/pe/telco/recargas/TelcoRecargasApplication.java
@@ -2,8 +2,10 @@ package pe.telco.recargas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TelcoRecargasApplication {
 
 	public static void main(String[] args) {
